e5a37792a910fc69d0eeec8b7ebb2133d37d798c
taranpreet99 <taran99.singh@gmail.com>
2023-02-27 00:12:09 -0500

    Simple Rest API with Spring Boot

---
+++ spring-boot-1/src/main/java/com/springbootpractice/springboot1/Course.java
@@ -0,0 +1,45 @@
+package com.springbootpractice.springboot1;
+
+public class Course {
+
+    private long id;
+    private String name;
+    private String author;
+
+
+    //Constructor
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+
+    //getters
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+    //tostring
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
---
+++ spring-boot-1/src/main/java/com/springbootpractice/springboot1/CourseController.java
@@ -0,0 +1,22 @@
+package com.springbootpractice.springboot1;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+public class CourseController {
+
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn AWS", "Someone"),
+                new Course(2,"Learn DevOps", "Someone")
+        );
+    }
+}

c6b16ca11be034ab265e9b29cf6948897c5d53e4
taranpreet99 <taran99.singh@gmail.com>
2023-04-02 20:12:01 -0400

    Added exception Handling

--- restful_web_services/src/main/java/com/webservices/restful_web_services/user/UserResource.java
+++ restful_web_services/src/main/java/com/webservices/restful_web_services/user/UserResource.java
@@ -26,7 +26,11 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user==null)
+            throw new UserNotFoundException("id:"+id);
+        return user;
     }
 
    @PostMapping("/users")

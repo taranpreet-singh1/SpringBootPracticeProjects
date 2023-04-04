9cfddd47013d272c58b49395c6657d06d9ceb229
taranpreet99 <taran99.singh@gmail.com>
2023-04-04 19:11:43 -0400

    Internationalization & Documenting Rest API

--- restful_web_services/src/main/java/com/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful_web_services/src/main/java/com/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,9 +1,20 @@
 package com.webservices.restful_web_services.helloworld;
 
+import org.aspectj.bridge.Message;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     @GetMapping( path = "/hello-world")
     public String helloWorld(){
@@ -19,6 +30,15 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World,%s",  name));
     }
+
+    @GetMapping( path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null,"Default Message", locale
+        );
+
+    }
+
 
 
 }

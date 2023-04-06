466055060a31bfe8d696bbbc24f2104b70d4e70f
taranpreet99 <taran99.singh@gmail.com>
2023-04-05 20:30:09 -0400

    Versioning, Static Filtering, HATEOAS

--- restful_web_services/src/main/java/com/webservices/restful_web_services/user/UserResource.java
+++ restful_web_services/src/main/java/com/webservices/restful_web_services/user/UserResource.java
@@ -2,9 +2,13 @@ package com.webservices.restful_web_services.user;
 
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.net.URI;
 import java.util.List;
@@ -26,12 +30,17 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if(user==null)
             throw new UserNotFoundException("id:"+id);
-        return user;
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
     }
 
    @PostMapping("/users")
---
+++ restful_web_services/src/main/java/com/webservices/restful_web_services/versioning/PersonV2.java
@@ -0,0 +1,10 @@
+package com.webservices.restful_web_services.versioning;
+
+public class PersonV2 {
+
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+}

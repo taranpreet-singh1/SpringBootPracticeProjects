12d240fb6359a9e40514e98337045c9117448f91
taranpreet99 <taran99.singh@gmail.com>
2023-04-07 13:31:50 -0400

    actuator & Hal Explorer

--- restful_web_services/src/main/java/com/webservices/restful_web_services/user/User.java
+++ restful_web_services/src/main/java/com/webservices/restful_web_services/user/User.java
@@ -1,14 +1,19 @@
 package com.webservices.restful_web_services.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name="user_details")
 public class User {
 
-
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message="Name should have atleast 2 characters")
     @JsonProperty("user_name")
---
+++ restful_web_services/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user_details(id,birth_date,name)
+values(1001,current_date(),'Taran');
+
+insert into user_details(id,birth_date,name)
+values(1002,current_date(),'Some Person');
+
+insert into user_details(id,birth_date,name)
+values(1003,current_date(),'Other Person');
+

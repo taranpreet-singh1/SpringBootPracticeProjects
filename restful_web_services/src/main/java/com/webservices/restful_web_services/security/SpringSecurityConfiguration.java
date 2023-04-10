7559eeaf33bc50878a3c61bd025f38428b751861
taranpreet99 <taran99.singh@gmail.com>
2023-04-10 18:41:36 -0400

    Added spring security filter chain

---
+++ restful_web_services/src/main/java/com/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.webservices.restful_web_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //Any request should be authenticated
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+}

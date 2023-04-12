d074723e3f5591eb0c9b29688c680cac8aa85c4e
taranpreet99 <taran99.singh@gmail.com>
2023-04-11 20:00:27 -0400

    React todo app

---
+++ spring-boot-practice2/src/test/java/com/taranspring/springbootpractice2/SpringBootPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taranspring.springbootpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
